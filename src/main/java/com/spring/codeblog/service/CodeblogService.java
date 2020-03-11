87063da275516dc34fee5da741cbbfc75239528d
Ivan Luis <ivanluisg20@gmail.com>
2020-03-11 20:22:51 -0300

    Commit 2

---
+++ src/main/java/com/spring/codeblog/service/CodeblogService.java
@@ -0,0 +1,16 @@
+package com.spring.codeblog.service;
+
+import java.util.List;
+
+import com.spring.codeblog.model.Post;
+
+public interface CodeblogService {
+	List<Post> findAll();
+	Post findById(Long id);
+	Post salvar(Post post);
+	
+	
+	
+	
+	
+}

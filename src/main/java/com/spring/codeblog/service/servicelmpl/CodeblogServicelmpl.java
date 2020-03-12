6721d046e914ff2a4a2c5a991841d8540566fccf
Ivan Luis <ivanluisg20@gmail.com>
2020-03-11 21:01:01 -0300

    commit 4

--- src/main/java/com/spring/codeblog/service/servicelmpl/CodeblogServicelmpl.java
+++ src/main/java/com/spring/codeblog/service/servicelmpl/CodeblogServicelmpl.java
@@ -1,5 +1,36 @@
 package com.spring.codeblog.service.servicelmpl;
 
-public class CodeblogServicelmpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.BlogRepository;
+import com.spring.codeblog.service.CodeblogService;
+
+@Service
+public class CodeblogServicelmpl implements CodeblogService{
+	
+	@Autowired
+	BlogRepository blogrepository;
+
+	@Override
+	public List<Post> findAll() {
+		// TODO Auto-generated method stub
+		return blogrepository.findAll();
+	}
+
+	@Override
+	public Post findById(Long id) {
+		// TODO Auto-generated method stub
+		return blogrepository.findById(id).get();
+	}
+
+	@Override
+	public Post salvar(Post post) {
+		// TODO Auto-generated method stub
+		return blogrepository.save(post);
+	}
 
 }
